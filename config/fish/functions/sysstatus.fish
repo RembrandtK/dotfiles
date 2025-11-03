0d52e47ca13d7e8afbb5874caa9fa8a076b0da50
Rembrandt Kuipers <50174308+RembrandtK@users.noreply.github.com>
2025-11-03 11:18:07 +0000

    Clean up test message

--- config/fish/functions/sysstatus.fish
+++ config/fish/functions/sysstatus.fish
@@ -1,7 +1,6 @@
 function sysstatus --description 'Show system status'
     set -l system_status /git/RembrandtK/rem/bin/system-status.sh
     if test -x $system_status
-        echo "🔗 Live dotfiles working! Running system status:"
         $system_status
     else
         echo "System status script not found at $system_status"
